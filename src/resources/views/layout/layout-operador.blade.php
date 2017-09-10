bccb45ca5dbe2cdf2edcbb4c2ca1e244d1839f4b
jonilsondeveloper <jonilsons9@gmail.com>
2017-09-10 09:35:42 -0300

    correcao de erro de pull, adicao da pasta vend/

--- src/resources/views/layout/layout-operador.blade.php
+++ src/resources/views/layout/layout-operador.blade.php
@@ -7,10 +7,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<!-- VENDOR CSS -->
-	<link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/vendor/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/vendor/linearicons/style.css">
-	<link rel="stylesheet" href="/vendor/chartist/css/chartist-custom.css">
+	<link rel="stylesheet" href="/vend/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/vend/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/vend/linearicons/style.css">
+	<link rel="stylesheet" href="/vend/chartist/css/chartist-custom.css">
 	<!-- MAIN CSS -->
 	<link rel="stylesheet" href="/css/main.css">
 	<link rel="stylesheet" href="/css/my-style.css">
@@ -122,11 +122,11 @@
 	</div>
 	<!-- END WRAPPER -->
 	<!-- Javascript -->
-	<script src="/vendor/jquery/jquery.min.js"></script>
-	<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-	<script src="/vendor/chartist/js/chartist.min.js"></script>
+	<script src="/vend/jquery/jquery.min.js"></script>
+	<script src="/vend/bootstrap/js/bootstrap.min.js"></script>
+	<script src="/vend/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="/vend/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+	<script src="/vend/chartist/js/chartist.min.js"></script>
 	<script src="/scripts/klorofil-common.js"></script>
 	<script src="/scripts/script.js"></script>
 
